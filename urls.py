92c325e68e7b9d7ca342ff6653b89e94f09d2c91
Anwesha Mishra <89926118+anweshamishra10@users.noreply.github.com>
2023-05-18 21:06:41 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,28 @@
+"""web_app URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from authentication import views
+
+handler404 = views.error_404_view
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('create-account/', views.create_account_view, name='create_account'),
+    path('verification/', views.verification_view, name='verification'),
+    path('account-created/', views.account_created_view, name='account_created'),
+]
+
